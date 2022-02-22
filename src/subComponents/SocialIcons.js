7b26945537e48106c7e7fc057d5180f076e0dbb9
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-22 11:45:17 +0200

    Remove unused social icons

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
-import { Facebook, Github, YouTube } from '../components/AllSvgs'
+import { Github } from '../components/AllSvgs'
 import { ReactComponent as LindedIcon } from '../assets/svg/linked-in.svg'
 import { DarkTheme } from '../components/Themes'
 
