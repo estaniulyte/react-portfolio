59f3e1cabf5f295d54674ea3d693aabbfa4ecae4
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-22 23:48:10 +0200

    Update Projects data

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -28,18 +28,18 @@ export const Blogs = [
   {
     id:4,
     name:"Job search app | UI/UX courses",
-    tags:["Figma","UI/UX","Design","InVision"],
+    tags:["Figma","UI/UX","Design","InVision", "Balsamiq"],
     date:"UI/UX process creating Job Searching application",
     imgSrc:"https://drive.google.com/uc?export=download&id=1SvPNWwYeeNusAMn-tQ99D7h96_vDJPrb",
     link:"https://evelinastaniulyte722314.invisionapp.com/public/prototype/sks5o8fvp008gc601y2pdggyl/3cfe6a04"
   },
   {
     id:5,
-    name:"Portrait Photography",
+    name:"Photography",
     tags:["Lightroom","Photoshop","Canon"],
     date:"",
     imgSrc:"https://drive.google.com/uc?export=download&id=1q3lsu_rfgSBpNsvOoMaGNrAYoo3bh2IV",
-    link:""
+    link:"https://www.instagram.com/ewastan__/"
   },
   {
     id:6,
@@ -47,41 +47,6 @@ export const Blogs = [
     tags:["Illustrator","Photoshop"],
     date:"",
     imgSrc:"https://drive.google.com/uc?export=download&id=1T0l99lliYeoh02wN3rBw-JoYPLpXurLV",
-    link:""
-  },
-    // {
-    //     id:5,
-    //     name:"What is Higher Order Component (HOC) in React JS?",
-    //     tags:["react","reactJS","web-development"],
-    //     date:"3 Feb, 2021",
-    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //     link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
-    // },
-    // {
-    //     id:6,
-    //     name:"How to implement Pagination Component in React from scratch",
-    //     tags:["react","css"],
-    //     date:"5 Jan, 2021",
-    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
-    //     link:"https://codebucks.hashnode.dev/pagination-in-react"
-
-    // },
-   
-    // {
-    //     id:7,
-    //     name:"What is Redux ? Simply Explained!",
-    //     tags:["react","redux","javascript"],
-    //     date:"16 May, 2021",
-    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
-    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
-    // },
-    // {
-    //     id:8,
-    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
-    //     tags:["react","framer-motion","styled-components"],
-    //     date:"26 May, 2021",
-    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
-    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
-    // },
-  
+    link:"https://www.instagram.com/ehvehleena/"
+  }
 ] 
