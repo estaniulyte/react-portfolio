b96c78351461709b73eddb9413b46a12457f99a9
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-22 23:48:33 +0200

    Update About page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,7 +5,7 @@ import LogoComponent from '../subComponents/LogoComponent';
 import SocialIcons from '../subComponents/SocialIcons';
 import ParticleComponent from '../subComponents/ParticleComponent';
 import BigTitle from '../subComponents/BigTitlte'
-import astronaut from '../assets/Images/spaceman.png'
+import astronaut from '../assets/Images/dev-female.png'
 
 const Box = styled.div`
   background-color: ${props => props.theme.body};
@@ -21,33 +21,147 @@ const float = keyframes`
 `
 const Spaceman = styled.div`
   position: absolute;
-  top: 10%;
-  right: 5%;
+  top: 20%;
+  right: 9%;
   width: 20vw;
   animation: ${float} 4s ease infinite;
   img {
       width: 100%;
       height: auto;
+      transform: rotate(-8deg);
   }
 `
 const Main =  styled.div`
   border: 2px solid ${(props) => props.theme.text};
   color: ${(props) => props.theme.text};
-  padding: 2rem;
-  width: 50vw;
-  height: 60vh;
+  padding: 5rem;
+  box-sizing: border-box;
+  width: 44vw;
+  height: 75vh;
   z-index: 3;
   line-height: 1.5;
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  font-size: calc(0.6rem + 1vw);
+  align-content: center;
+  font-size: calc(1vw);
   backdrop-filter: blur(4px);
   position: absolute;
-  left: calc(5rem + 5vw);
+  left: calc(6rem + 5vw);
   top: 10rem;
-  font-family: 'Ubuntu Mono', monospace;
-  font-style: italic;
+  font-family: Inter, Roboto;
+
+  @media (max-width: 600px) {
+    font-size: calc(3.6vw);
+    padding: 1rem;
+    width: 80vw;
+    height: 85vh;
+    top: 6rem;
+    left: calc(1.7rem + 5vw);
+  }
+
+  .box {
+    padding: 5px 8px;
+    margin: 4px;
+    padding: 12px;
+    margin-right: 8px;
+    font-size: calc(0.8vw);
+    cursor: default;
+
+    @media (max-width: 600px) {
+      padding: 1px 2px;
+      font-size: calc(3vw);
+      margin-right: 1px;
+    }
+  }
+
+  .color1 {
+    background-color: #a32929;
+  }
+
+  .color2 {
+    background-color: #a34a29;
+  }
+
+  .color3 {
+    background-color: #a36a29;
+  }
+
+  .color4 {
+    background-color: #a38a29;
+  }
+
+  .color5 {
+    background-color: #98a329;
+  }
+
+  .color6 {
+    background-color: #77a329;
+  }
+
+  .color7 {
+    background-color: #55a329;
+  }
+
+  .color8 {
+    background-color: #34a329;
+  }
+
+  .color9 {
+    background-color: #29a33f;
+  }
+
+  .color10 {
+    background-color: #29a360;
+  }
+
+  .color11 {
+    background-color: #29a382;
+  }
+
+  .color12 {
+    background-color: #29a3a3;
+  }
+
+  .color13 {
+    background-color: #2982a3;
+  }
+
+  .color14 {
+    background-color: #2960a3;
+  }
+
+  .color15 {
+    background-color: #293fa3;
+  }
+
+  .color16 {
+    background-color: #3429a3;
+  }
+
+  .color17 {
+    background-color: #5529a3;
+  }
+
+  .color18 {
+    background-color: #7729a3;
+  }
+
+  .color19 {
+    background-color: #9829a3;
+  }
+
+  .inline-box{
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    margin:0;
+    margin-top: 7px;
+    padding:0;
+    text-align: left;
+    align-items: left;
+    flex-wrap: wrap;
+  }
 `
 
 const AboutPage = () => {
@@ -61,8 +175,40 @@ const AboutPage = () => {
             <img src={astronaut} alt="spaceman" />
         </Spaceman>    
         <Main>
-          <br /> <br/>
-          <br/> <br/>
+          <p>Hello! I'm Evelina Staniulyte, a experienced <b>Software Test engineer</b> with <b>UI & UX</b> expertise and <b>Front End development</b> skills.</p>
+          <br />
+          <p>I mainly work with the Unity engine since I've been working for Unity Technologies over 3 years.</p>
+          <br/> 
+          <p>I finished bachelor studies of Multimedia and Computer design, later UI/UX courses, Black Box testing foundations and the latest is Front-End academy. The learning path doesn't end here, because a part of leisure time I spend on improving coding skills.</p>
+          <br/>
+          Things I have experience with: 
+          <br/>
+          <div className="inline-box">
+            <div className="color1 box">JavaScript</div>
+            <div className="color2 box">React</div>
+            <div className="color4 box">VueJS</div>
+            <div className="color5 box">Html</div>
+            <div className="color6 box">CSS</div>
+            <div className="color7 box">SASS</div>
+            <div className="color8 box">NodeJS</div>
+            <div className="color9 box">GIT</div>
+          </div>
+          <div className="inline-box">
+            <div className="color10 box">Unity Engine</div>
+            <div className="color11 box">C#</div>
+            <div className="color12 box">TestRail</div>
+            <div className="color13 box">JIRA</div>
+            <div className="color14 box">Confluence</div>
+            <div className="color15 box">SQL</div>
+            <div className="color16 box">FogBugz</div>
+          </div>
+          <div className="inline-box">
+            <div className="color17 box">Figma</div>
+            <div className="color18 box">Balsamiq</div>
+            <div className="color19 box">Bitbucket</div>
+          </div>
+          <br/>
+        <p>Outside of work, I enjoy practising dances like Salsa, Rueda, and other Latino styles. Also, I spend much time taking pictures and even more on retouching them later.</p>
         </Main>
         <BigTitle text="ABOUT" top="10%" left="5%" />
       </Box>
