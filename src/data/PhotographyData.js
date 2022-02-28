9bb9c72a671e83dc5e9ff63f163f87518349b20e
evelinastaniulyte <43463977+estaniulyte@users.noreply.github.com>
2022-02-28 23:43:22 +0200

    Create PhotographyData.js

---
+++ src/data/PhotographyData.js
@@ -0,0 +1,94 @@
+export const Photos = [
+  {
+    id:1,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1VPBrG-gYP_mdAuOhgoLgEnArexgU5jx-",
+  },
+  {
+    id:2,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1q3lsu_rfgSBpNsvOoMaGNrAYoo3bh2IV",
+  },
+  {
+    id:3,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1Eaxl2cAuk8neZObKkLMGcT6heNRsmoOX",
+  },
+  {
+    id:4,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1_fKcekg04XtsZbk6N4AmFWMH3zRNseFm",
+  },
+  {
+    id:5,
+    imgSrc:"https://drive.google.com/uc?export=download&id=13tmSMgqxYXdP7r6tKmX78u4ets9jM40e",
+  },
+  {
+    id:6,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1va3BVvhAdDiS94mfHDylGIjFjfAp5keL",
+  },
+  {
+    id:7,
+    imgSrc:"https://drive.google.com/uc?export=download&id=154X2zQas7fwiWWy77bgsY7ProGlv6hbn",
+  },
+  {
+    id:8,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1qLsLXIi3OLUYVTRgtgh_WnkDzBldgQPQ",
+  },
+  {
+    id:9,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1MEjXYCo71MzwMx5rKIWCiKH1auFE2OAB",
+  },
+  {
+    id:9,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1PWF1TAqPSuQwcMaLsuECHSs0Xn9TqJ9G",
+  },
+  {
+    id:10,
+    imgSrc:"https://drive.google.com/uc?export=download&id=1OKNMdGA2Tqyl85wS6Buw1k7Osmm0arjC",
+  },
+  {
+    id: 11,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1wMadjCxw36P-KeVc3s19UPQgsauqiJ31"
+  },
+  {
+    id: 12,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1Pxc9jGe_fw5yeYmdIOV0trpOCRAY-dXW"
+  },
+  {
+    id: 13,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1k0gKz2RlOg60Fe12KhWuKaZQrWMBq4Nm"
+  },
+  {
+    id: 14,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1NmEdXK77LRYrZCbxy_33cNHWjYAN3sX4"
+  },
+  {
+    id: 15,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1RS3fQ1oC-xbttYiP3KOQdkwGOY0YJzy-"
+  },
+  {
+    id: 16,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1DslmN_fYASE3Bmv9eQVlouJznA7jUeUa"
+  },
+  {
+    id: 17,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1XIFLLIWj4COD4AYUhdxQKcpf-O1qTLr9"
+  },
+  {
+    id: 18,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1V0t0jw3pat-MbYWxbawdahHOJYkTnWd3"
+  },
+  {
+    id: 19,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1tMM67t2EjQyQ1zyDvYEF0UK4yfkCLpk_"
+  },
+  {
+    id: 20,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1h4k4jtkmOW4cP-SyLSlzFUmDsSW8n1Uy"
+  },
+  {
+    id: 21,
+    imgSrc: "https://drive.google.com/uc?export=download&id=160EmBJnTIrS90N9P2uIthHF7iDMJPY8z"
+  },
+  {
+    id: 22,
+    imgSrc: "https://drive.google.com/uc?export=download&id=1cew7VN52amsTt-DOZ5Y0iJNTyUlg3Ws1"
+  },
+] 
